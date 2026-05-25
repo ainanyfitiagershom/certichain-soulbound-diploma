fffd97479c396da552a3d1bf01fa435236f47456
kanto28 <97189380+kanto28@users.noreply.github.com>
2026-05-25 21:25:54 +0300

    add OpenZeppelin dependencies

--- contracts/DiplomaSoulbound.sol
+++ contracts/DiplomaSoulbound.sol
@@ -1,6 +1,13 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract DiplomaSoulbound {
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract DiplomaSoulbound is ERC721, Ownable {
+
+    constructor() ERC721("CertiChainDiploma", "CCD") Ownable(msg.sender) {
+
+    }
 
 }
