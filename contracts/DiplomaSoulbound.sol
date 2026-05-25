6121205d6e77a337680884bceb9b21e45aa12492
kanto28 <97189380+kanto28@users.noreply.github.com>
2026-05-25 21:32:58 +0300

    add diploma structure and storage

--- contracts/DiplomaSoulbound.sol
+++ contracts/DiplomaSoulbound.sol
@@ -5,9 +5,29 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract DiplomaSoulbound is ERC721, Ownable {
-
-    constructor() ERC721("CertiChainDiploma", "CCD") Ownable(msg.sender) {
-
+    struct Diploma {
+        string studentName;
+        string diplomaName;
+        string year;
+        string mention;
+        uint256 issuedAt;
+        bool revoked;
     }
 
+    mapping(uint256 => Diploma) private diplomas;
+
+    uint256 private nextTokenId;
+
+    event DiplomaIssued(
+        uint256 indexed tokenId,
+        address indexed student
+    );
+
+    event DiplomaRevoked(
+        uint256 indexed tokenId
+    );
+
+    constructor() ERC721("CertiChainDiploma", "CCD") Ownable(msg.sender) {
+        nextTokenId = 1;
+    }
 }
